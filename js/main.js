a1f12565eea867f24428a2429cd306cbf5aa07e7
theddn <theddn@gmail.com>
2024-12-22 01:00:40 +0200

    Data Model

--- js/main.js
+++ js/main.js
@@ -1,2 +1,14 @@
 'use strict'
 
+var gImgs = [{ id: 1, url: 'meme-imgs/1.jpg' }]
+var gMeme = {
+    selectedImgId: 5,
+    selectedLineIdx: 0,
+    lines: [
+        {
+            txt: 'I sometimes eat Falafel',
+            size: 20,
+            color: 'red',
+        },
+    ],
+}
