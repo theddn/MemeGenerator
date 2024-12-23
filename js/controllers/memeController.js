3216938df8b9800c72cff4d8245ebda5ba1d094c
theddn <theddn@gmail.com>
2024-12-23 22:31:53 +0200

    Add  drawText() and renderGallery()

--- js/controllers/memeController.js
+++ js/controllers/memeController.js
@@ -3,14 +3,12 @@
 var gElCanvas
 var gCtx
 
-
 function onInit() {
     gElCanvas = document.querySelector('canvas')
     gCtx = gElCanvas.getContext('2d')
-
+    renderGallery()
     renderMeme()
 }
-
 
 function renderMeme() {
     const currMeme = getMeme()
@@ -18,5 +16,39 @@ function renderMeme() {
     const elImg = new Image()
 
     elImg.src = `meme-imgs/${selectedImgId}.jpg`
-    gCtx.drawImage( elImg, 0, 0, gElCanvas.width, gElCanvas.height)
+
+    elImg.onload = () => {
+        gCtx.drawImage(elImg, 0, 0, gElCanvas.width, gElCanvas.height)
+        drawText()
+    }
+}
+
+function renderGallery() {
+    const imgs = getImgs()
+    let strHtml = imgs.map((img) => {
+        return `<img  class="gallery-item" src="${img.url}" onclick="onImgSelect(${img.id})">`
+    })
+    document.querySelector('.gallery').innerHTML = strHtml.join('')
+}
+function onImgSelect(ImgId) {
+    setImgMeme(ImgId)
+    renderMeme()
+}
+function drawText() {
+    const linesText = getLineTxt()
+    linesText.forEach((lineText, idx) => {
+        const { txt, size, color, pos } = lineText
+        const { x, y } = pos
+
+        gCtx.font = '40px Ariel'
+
+        gCtx.strokeStyle = 'black'
+        gCtx.fillStyle = 'red'
+        gCtx.lineWidth = '10'
+
+        gCtx.textAlign = 'center'
+
+        gCtx.strokeText(txt, x, y)
+        gCtx.fillText(txt, x, y)
+    })
 }
--- js/services/memeService.js
+++ js/services/memeService.js
@@ -1,14 +1,35 @@
 'use strict'
 
-var gImgs = [{ id: 1, url: 'meme-imgs/1.jpg' }]
+var gImgs = [
+    { id: 1, url: 'meme-imgs/1.jpg' },
+    { id: 2, url: 'meme-imgs/2.jpg' },
+    { id: 3, url: 'meme-imgs/3.jpg' },
+    { id: 4, url: 'meme-imgs/4.jpg' },
+    { id: 5, url: 'meme-imgs/5.jpg' },
+    { id: 6, url: 'meme-imgs/6.jpg' },
+    { id: 7, url: 'meme-imgs/7.jpg' },
+    { id: 8, url: 'meme-imgs/8.jpg' },
+    { id: 9, url: 'meme-imgs/9.jpg' },
+    { id: 10, url: 'meme-imgs/10.jpg' },
+    { id: 11, url: 'meme-imgs/11.jpg' },
+    { id: 12, url: 'meme-imgs/12.jpg' },
+    { id: 13, url: 'meme-imgs/13.jpg' },
+    { id: 14, url: 'meme-imgs/14.jpg' },
+    { id: 15, url: 'meme-imgs/15.jpg' },
+    { id: 16, url: 'meme-imgs/16.jpg' },
+    { id: 17, url: 'meme-imgs/17.jpg' },
+    { id: 18, url: 'meme-imgs/18.jpg' },
+]
 var gMeme = {
     selectedImgId: 1,
     selectedLineIdx: 0,
     lines: [
         {
             txt: 'I sometimes eat Falafel',
-            size: 20,
+            size: 40,
+            font: 'Ariel',
             color: 'red',
+            pos: { x: 100, y: 50 },
         },
     ],
 }
@@ -17,7 +38,13 @@ function getMeme() {
     return gMeme
 }
 
-function getSelectedImgId() {
-    return gMeme.selectedImgId
+function getImgs() {
+    return gImgs
+}
+function getLineTxt() {
+    return gMeme.lines
 }
 
+function setImgMeme(ImgId) {
+    gMeme.selectedImgId = ImgId
+}
